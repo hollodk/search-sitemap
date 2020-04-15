133c3323b12facd5bba02ac0478ee5115fbbeab9
Michael Holm <hollo@hollo.dk>
2020-04-15 21:48:36 +0200

    added search controller

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    /**
+     * @Route("")
+     */
+    public function index(Request $request)
+    {
+        $sitemap = file_get_contents($_ENV['SITEMAP_URL']);
+        $query = $this->clean($request->get($_ENV['QUERY_PARAMETER']));
+
+        $xml = new \SimpleXMLElement($sitemap);
+        $res = [];
+        $last = null;
+
+        foreach ($xml->url as $url) {
+            $search = $this->clean($url->loc);
+
+            if (preg_match("/".$query."/", $search)) {
+                $last = $url->loc;
+
+                $res[] = $last;
+            }
+        }
+
+        if ($last) {
+            return $this->redirect($last);
+        } else {
+            return $this->redirect($request->server->get('HTTP_REFERER'));
+        }
+    }
+
+    private function clean($input)
+    {
+        $input = preg_replace("/\-/", "", $input);
+        $input = preg_replace("/http:\/\//", "", $input);
+        $input = preg_replace("/https:\/\//", "", $input);
+        $input = preg_replace("/\//", "", $input);
+
+        return $input;
+    }
+}
